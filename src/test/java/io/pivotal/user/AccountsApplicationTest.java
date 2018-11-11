ed29104431303afdd350928fc351c48f3914ff2b
Simon Rowe <srowe@pivotal.io>
2018-11-11 07:46:14 +0000

    update to spring boot 2.

--- src/test/java/io/pivotal/user/AccountsApplicationTest.java
+++ src/test/java/io/pivotal/user/AccountsApplicationTest.java
@@ -4,18 +4,16 @@ import io.pivotal.user.UserApplication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Tests for the Accounts Application.
  * @author David Ferreira Pinto
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UserApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserApplication.class)
 public class AccountsApplicationTest {
 	/**
 	 * test loading of spring context.
